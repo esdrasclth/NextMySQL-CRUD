de2861e9ec77d4392517274e1fa0dd72152b4341
Esdras Clother <Esdras.Clother@outlook.com>
2023-08-30 22:45:40 -0600

    conexion a la base de datos mysql

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
+import { conn } from "@/libs/mysql";
 
-export function GET() {
+export async function GET() {
+    const result = await conn.query('SELECT NOW()')
+    console.log(result)
     return NextResponse.json({ message: 'Hello World!' });
 }
