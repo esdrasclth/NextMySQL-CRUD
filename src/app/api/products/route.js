9d3779c804250f6a93273d63a45af2998c6aefcf
Esdras Clother <Esdras.Clother@outlook.com>
2023-08-31 22:20:46 -0600

    consulta SQL para listar los productos creadoso

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,8 +1,14 @@
 import { NextResponse } from "next/server";
 import { conn } from "@/libs/mysql";
 
-export function GET() {
-    return NextResponse.json({ message: "listando productos" });
+export async function GET() {
+    try {
+        const result = await conn.query("SELECT * FROM products")
+        return NextResponse.json(result);
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
 }
 
 export async function POST(request) {
