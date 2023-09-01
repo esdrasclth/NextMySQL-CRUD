9da83296d3718ba4a3e443e7f1f66728905fdbc0
Esdras Clother <Esdras.Clother@outlook.com>
2023-08-31 23:17:42 -0600

    consulta SQL para eleminar productos de la BD

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,11 +1,36 @@
 import { NextResponse } from "next/server";
+import { conn } from "@/libs/mysql";
 
-export function GET() {
-    return NextResponse.json({ message: 'obteniendo un producto' })
+export async function GET(request, { params }) {
+    try {
+        const result = await conn.query("SELECT * FROM products WHERE id = ?", [params.id]);
+
+        if (result.length === 0) {
+            return NextResponse.json({
+                message: "Producto no encontrado"
+            }, { status: 404 })
+        }
+
+        return NextResponse.json(result[0]);
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
 }
 
-export function DELETE() {
-    return NextResponse.json({ message: 'eliminando un producto' })
+export async function DELETE(request, { params }) {
+    try {
+        const result = await conn.query("DELETE FROM products WHERE id = ?", [params.id]);
+
+        if (result.affectedRows === 0) {
+            return NextResponse.json({ message: "Product no encontrado" }, { status: 404 })
+        }
+
+        return new Response(null, { status: 204 })
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
 }
 
 export function PUT() {
