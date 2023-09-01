33b486891728c79d1fc8d3af2d9e34bec3b0c03d
Esdras Clother <Esdras.Clother@outlook.com>
2023-08-31 22:08:07 -0600

    consulta SQL para la cracion de nuevos producto

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,9 +1,28 @@
 import { NextResponse } from "next/server";
+import { conn } from "@/libs/mysql";
 
 export function GET() {
-    return NextResponse.json({ message: 'listando productos' })
+    return NextResponse.json({ message: "listando productos" });
 }
 
-export function POST() {
-    return NextResponse.json({ message: 'creando productos' })
+export async function POST(request) {
+    try {
+        const { name, description, price } = await request.json();
+
+        const result = await conn.query("INSERT INTO products SET ?", {
+            name,
+            description,
+            price,
+        });
+
+        return NextResponse.json({
+            name,
+            description,
+            price,
+            id: result.insertId
+        });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
 }
